5a6192515d5c814230d9ea0e3b95dfcd97dcb4b6
V1nicius28 <viniciusgarcezmachado@gmail.com>
2025-12-08 06:56:45 -0300

    refactor: remove perguntas

--- src/main/java/petsystem/repository/PerguntaRepository.java
+++
@@ -1,7 +0,0 @@
-package petsystem.repository;
-
-import petsystem.model.Pergunta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerguntaRepository extends JpaRepository<Pergunta, Long> {
-}
